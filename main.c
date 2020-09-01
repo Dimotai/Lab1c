01d3339052435d0b30f13d000df16a42245dc756
Dimotai <dimotaimusic@gmail.com>
2020-09-01 17:54:20 +0000

    Lolx3

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 
 int main(void) {
   double temp;
-  printf("Enter temperature in celsius: ");
-  scanf("\n%lf", &temp); 
+  printf("Enter temperature in celsius:\n");
+  scanf("%lf", &temp); 
   double ftemp = (temp*9/5)+32;
   printf("%lf° in Celsius is equivalent to %lf° Fahrenheit.", temp, ftemp);
   return 0;
