8a6bda4cb57d9637f8b2eb52f4708e81ebe52da7
Dimotai <dimotaimusic@gmail.com>
2020-09-01 17:34:54 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <readline/readline.h>
+
+int main(void) {
+  double temp;
+  printf("Enter temperature in celsius: ");
+  scanf("%lf", &temp); 
+  double ftemp = (temp*9/5)+32;
+  printf("%lf° in Celsius is equivalent to %lf° Fahrenheit.", temp, ftemp);
+  return 0;
+}
